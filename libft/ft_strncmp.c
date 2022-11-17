1ac445c7899ab0798428e0857c00e23dd30cd55a
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-11-17 14:51:38 -0300

    Add files via upload

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 14:14:41 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/11/16 13:40:19 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/11/17 14:49:57 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t			i;
 	unsigned char	*str;
 	unsigned char	*str2;
-
 
 	i = 0;
 	str = (unsigned char *)s1;
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 11:24:51 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/11/17 14:33:22 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/11/17 14:50:27 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ typedef struct stack
 
 void	check_num(char *num);
 void	check_limit(long len);
-void	ft_stack(char **value,t_stack *stack);
+void	ft_stack(char **value, t_stack *stack);
 void	check_repeat(int len, t_stack *stack);
 
 #endif
