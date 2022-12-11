fc4ce1cf6396f6b8a9cb35e4d72296e762701baf
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-12-11 15:00:08 -0300

    Add files via upload

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 21:39:41 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/12/10 21:53:32 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/12/11 14:58:44 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,52 @@ void    pa(t_stack *stack)
         i--;
     }
     stack->len_a++;
-    clean_b(stack);
+    stack->num_a[0] = stack->num_b[0];
+    if(stack->len_b > 1)
+        clean_b(stack);
+    else
+        free(stack->num_b);
+}
+
+void    clean_b(t_stack *stack)
+{
+    int i;
+    int *temp;
+
+    i = 1;
+    temp = malloc(sizeof(int) * (stack->len_a - 1));
+    while (i  < stack->len_a)
+    {
+        temp[i - 1] = stack->num_a[i];
+        i++;
+    }
+    free(stack->num_a);
+    stack->len_a--;
+    stack->num_a = malloc(sizeof(int) * stack->len_a);
+    i = 0;
+    while (i  < stack->len_a)
+    {
+        stack->num_a[i] = temp[i];
+        i++;
+    }
+    free(temp);
+}
+
+void    pb(t_stack *stack)
+{
+    int i;
+
+    i = stack->len_b + 1;
+    stack->num_b = malloc(sizeof(int));
+    while (i > 0)
+    {
+        stack->num_b[i] = stack->num_b[i - 1];
+        i--;
+    }
+    stack->len_b++;
+    stack->num_b[0] = stack->num_a[0];
+    if(stack->len_a > 1)
+        clean_a(stack);
+    else
+        free(stack->num_a);
 }
