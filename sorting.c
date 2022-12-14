e7cc130d0cad1455014d1ff7687f41a0fc69fbea
Marcus Vinicius Pimenta <57456066+viniiipimenta@users.noreply.github.com>
2022-12-14 20:23:21 -0300

    Add files via upload

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 15:58:46 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/12/14 17:26:06 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/12/14 20:21:58 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,35 @@
 
 void	sort_thee(t_stack *stack)
 {
-	if (stack->num_a[0] == 1 && stack->num_a[1] == 0)
+	if (stack->num_a[0] > stack->num_a[1] && stack->num_a[0] < stack->num_a[2])
 		sa(1, stack);
-	else if (stack->num_a[0] == 1 && stack->num_a[1] == 2)
+	else if (stack->num_a[0] < stack->num_a[1] && stack->num_a[0] > stack->num_a[2])
 		rra(1, stack);
-	else if (stack->num_a[0] == 0)
+	else if (stack->num_a[0] < stack->num_a[1] && stack->num_a[1] > stack->num_a[2])
 	{
 		sa(1, stack);
 		ra(1, stack);
 	}
-	else if (stack->num_a[0] == 2 && stack->num_a[1] == 0)
+	else if (stack->num_a[0] > stack->num_a[1] && stack->num_a[1] < stack->num_a[2])
 		ra(1, stack);
-	else if (stack->num_a[0] == 2 && stack->num_a[1] == 1)
+	else if (stack->num_a[0] > stack->num_a[1] && stack->num_a[1] > stack->num_a[2])
 	{
 		sa(1, stack);
 		rra(1, stack);
 	}
+}
+
+void	sort_five(t_stack *stack)
+{
+	while (stack->num_a[0] != 0)
+		ra(1, stack);
+	pb(stack);
+	while (stack->num_a[0] != 1)
+		ra(1, stack);
+	pb(stack);
+	sort_thee(stack);
+	pa(stack);
+	pa(stack);
 }
 
 void	sorting(t_stack *stack)
@@ -41,4 +54,6 @@ void	sorting(t_stack *stack)
 		sa(1, stack);
 	else if (len == 3)
 		sort_thee(stack);
+	else if (len < 6)
+		sort_five(stack);
 }
