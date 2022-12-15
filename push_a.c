f29894fcbb3568c70a214b77ecb66ddec249c5ed
Marcus Vinicius Pimenta <57456066+viniiipimenta@users.noreply.github.com>
2022-12-14 22:41:00 -0300

    Add files via upload

--- push_a.c
+++ push_a.c
@@ -5,81 +5,81 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/10 21:39:41 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/12/13 17:58:43 by mpimenta         ###   ########.fr       */
+/*   Created: 2022/12/13 10:21:20 by mpimenta          #+#    #+#             */
+/*   Updated: 2022/12/14 19:54:07 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	moving(t_stack *stack)
+void	move(t_stack *stack)
 {
 	int	i;
 	int	*temp;
 
 	i = 1;
-	temp = malloc(sizeof(int) * (stack->len_a - 1));
-	while (i < stack->len_a)
-	{
-		temp[i - 1] = stack->num_a[i];
-		i++;
-	}
-	free(stack->num_a);
-	stack->len_a--;
-	stack->num_a = malloc(sizeof(int) * stack->len_a);
-	i = 0;
-	while (i < stack->len_a)
-	{
-		stack->num_a[i] = temp[i];
-		i++;
-	}
-	free(temp);
-}
-
-void	mount(t_stack *stack)
-{
-	int	i;
-	int	*temp;
-
-	i = 0;
-	temp = malloc(sizeof(int) * stack->len_b);
+	temp = malloc(sizeof(int) * (stack->len_b - 1));
 	while (i < stack->len_b)
 	{
-		temp[i] = stack->num_b[i];
+		temp[i - 1] = stack->num_b[i];
 		i++;
 	}
 	free(stack->num_b);
-	stack->len_b++;
+	stack->len_b--;
 	stack->num_b = malloc(sizeof(int) * stack->len_b);
-	stack->num_b[0] = stack->num_a[0];
-	i = 1;
+	i = 0;
 	while (i < stack->len_b)
 	{
-		stack->num_b[i] = temp[i - 1];
+		stack->num_b[i] = temp[i];
 		i++;
 	}
 	free(temp);
 }
 
-void	pb(t_stack *stack)
+void	mounting(t_stack *stack)
+{
+	int	i;
+	int	*temp;
+
+	i = 0;
+	temp = malloc(sizeof(int) * stack->len_a);
+	while (i < stack->len_a)
+	{
+		temp[i] = stack->num_a[i];
+		i++;
+	}
+	free(stack->num_a);
+	stack->len_a++;
+	stack->num_a = malloc(sizeof(int) * stack->len_a);
+	stack->num_a[0] = stack->num_b[0];
+	i = 1;
+	while (i < stack->len_a)
+	{
+		stack->num_a[i] = temp[i - 1];
+		i++;
+	}
+	free(temp);
+}
+
+void	pa(t_stack *stack)
 {
 	int	i;
 
-	i = stack->len_b + 1;
+	i = stack->len_a + 1;
 	if (i == 1)
 	{
-		stack->num_b = malloc(sizeof(int));
-		stack->num_b[0] = stack->num_a[0];
-		stack->len_b++;
+		stack->num_a = malloc(sizeof(int));
+		stack->num_a[0] = stack->num_b[0];
+		stack->len_a++;
 	}
 	else
-		mount(stack);
-	if (stack->len_a > 1)
-		moving(stack);
-	else if (stack->len_a == 1)
+		mounting(stack);
+	if (stack->len_b > 1)
+		move(stack);
+	else if (stack->len_b == 1)
 	{
-		stack->len_a--;
-		free(stack->num_a);
+		stack->len_b--;
+		free(stack->num_b);
 	}
 	ft_putstr_fd("pa\n", 1);
 }
